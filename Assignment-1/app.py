629e2c381cf2f4821684ed155c99a15926d180d4
venky3692 <iyervenkatesh92@gmail.com>
2025-03-09 08:25:56 +0530

    adding comments and code cleaning

--- Assignment-1/app.py
+++ Assignment-1/app.py
@@ -5,23 +5,24 @@ from langchain.vectorstores import Chroma
 from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException
 import config
-import chromadb
 import os
 
 
 app = FastAPI()
 os.environ["OPENAI_API_KEY"] = ""
 
+# Base model for text input
 class TextInput(BaseModel):
     text: str
 
 class Conversation:
     def __init__(self):
         self.hf_embedding = HuggingFaceEmbeddings(model_name = config.embedding_model_name)
-        self.vector_store = Chroma(collection_name= "document_collection", persist_directory="/home/venkys/Assignment/chroma_langchain_db", embedding_function=self.hf_embedding)
+        self.vector_store = Chroma(collection_name= config.name_of_collection, persist_directory=config.vector_store_path, embedding_function=self.hf_embedding)
         self.llm = OpenAI(temperature = config.llm_temperature)
         self.chat_history = []
     
+    # List to track chat history
     def get_chat_history(self, input):
         res = []
         for human, ai in input:
@@ -29,15 +30,16 @@ class Conversation:
             
         return "\n".join(res)
 
+    # Open AI api to converse. Vector store used as a retreiver
     def set_bot(self):
         retreiver = self.vector_store.as_retriever(search_kwargs={"k": 4})
-        print("RETREIVER", retreiver)
         self.bot_chat = ConversationalRetrievalChain.from_llm(self.llm, 
                                                               retreiver, 
                                                               return_source_documents=True,
                                                               get_chat_history = self.get_chat_history,
                                                               verbose=True)
     
+    # Prompts
     def generate_prompt(self, question):
         if not self.chat_history:
             prompt = f"You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. \nQuestion: {question}\nContext: \nAnswer:"
@@ -48,6 +50,7 @@ class Conversation:
         
         return prompt
 
+    # QnA 
     def ask_question(self, query):
         prompt = self.generate_prompt(query)
         result = self.bot_chat.invoke({"question": prompt, "chat_history": self.chat_history})
@@ -55,6 +58,7 @@ class Conversation:
 
         return result["answer"]
 
+# Fast API Endpoint
 @app.post("/start_chat")
 def start_conversation(input_data: TextInput):
     try:
--- Assignment-1/util.py
+++ Assignment-1/util.py
@@ -6,24 +6,21 @@ from langchain_core.documents import Document
 from pathlib import Path
 from uuid import uuid4
 import numpy as np
-import chromadb
 import config
-
 
 class Docutils:
     def __init__(self):
         self.loaded_docs = []
-        self.all_doc_text = [] # to store the document chunks
-        self.all_doc_embeddings = [] # to store the chunk embeddings
-        self.all_doc_metadata = [] # to store the document metada
-        self.all_doc_ids = [] # to store document id
-        self.hf_embeddings = HuggingFaceEmbeddings(model_name = config.embedding_model_name)
+        self.all_doc_text = [] # to store the Document objects
+        self.hf_embeddings = HuggingFaceEmbeddings(model_name = config.embedding_model_name) # HF light-weight embedding model
     
+    # Extract all the .md files within directories and sub-directories
     def get_all_files(self):
         all_docs = list(Path(config.directory_path).rglob(f"*.{config.file_pattern}"))
         
         return all_docs
 
+    # Loader to load all the .md documents
     def document_loader(self, docs):
         for item in docs:
             loader = UnstructuredMarkdownLoader(item)
@@ -31,6 +28,7 @@ class Docutils:
         
         return self.loaded_docs
 
+    # chunking the documents and creating Document objects
     def document_chunking(self, loaded_docs):
         text_splitter = CharacterTextSplitter(separator= config.separator_of_chunk,
                                               chunk_size = config.size_of_chunk,
@@ -39,39 +37,23 @@ class Docutils:
             all_doc_text = text_splitter.split_text(ele[0].page_content)
             for i, txt in enumerate(all_doc_text):
                 document = Document(page_content= txt, metadata={"source": ele[0].metadata['source']}, id = str(i))
-                # doc_metadata = ele[0].metadata['source']
-                # doc_embedding = self.hf_embeddings.embed_documents([txt])
-                # self.all_doc_text.extend([txt])
-                # self.all_doc_embeddings.extend(np.array(doc_embedding))
-                # self.all_doc_metadata.append({'source': doc_metadata})
-                # self.all_doc_ids.append(str(uuid4()))
                 self.all_doc_text.append(document)
-        # return self.all_doc_text, self.all_doc_embeddings, self.all_doc_metadata, self.all_doc_ids
         return self.all_doc_text
     
-    def create_collection(self, doc_text, doc_embeds, doc_metadata, doc_id):
-        # new_collection = self.client.create_collection(name=config.name_of_collection)
-        # new_collection.add(documents=doc_text, metadatas= doc_metadata, ids = doc_id, embeddings=doc_embeds)
-        # self.client.persist(config.vector_store_path)
-        presistant_storage = chromadb.PersistentClient(path= config.vector_store_path)
-        document_collection = presistant_storage.create_collection(name= config.name_of_collection,metadata={"hnsw:space":"cosine"})
-        document_collection.add(documents=doc_text, metadatas= doc_metadata, ids = doc_id, embeddings=doc_embeds)
-    
+    # Creating a vector store and saving it locally so that it can be used during conversation
     def create_vectorstore(self, doc_text):
         uuids = [str(uuid4()) for _ in range(len(doc_text))]
         vector_store = Chroma(
-        collection_name="document_collection",
+        collection_name=config.name_of_collection,
         embedding_function=self.hf_embeddings,
-        persist_directory="./chroma_langchain_db",  # Where to save data locally, remove if not necessary
-)
+        persist_directory=config.vector_store_path)
         vector_store.add_documents(documents=doc_text, ids=uuids)
-
-        results = vector_store.similarity_search("How can we do Authentication through the Serial Vault?", k=2)
-        print(results)
+        
+        print('Documents added successfully!')
 
 obj = Docutils()
 files = obj.get_all_files()
 loader = obj.document_loader(files)
 doc_txt= obj.document_chunking(loader)
 obj.create_vectorstore(doc_txt)
-print('Collection saved locally!')
+print('Vector store saved locally!')
